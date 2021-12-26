463d3c4dfff4c06790dfef7f11480d0a59c2790d
Omid Tahmasbi <91671463+OmidTahmasbi1994@users.noreply.github.com>
2021-12-27 03:00:10 +0330

    Add files via upload

---
+++ loop do....while.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+int main()
+
+{
+	int n;
+	int i=0;
+	
+	cout<<"Enter n:"<<endl;
+	cin>>n;
+	
+	long sum = 0;
+	
+	do
+	sum += i++ ;
+	
+	while(i <= n) ;
+	
+	cout<<"n : "<<n<<" sum : "<<sum;
+	
+	getch();
+}
